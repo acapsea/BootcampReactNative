b8cdb4af4896214008f6c9906bb1302e572bc42a
rbernanda <acappemenang@gmail.com>
2020-12-23 10:29:26 +0700

    Update Tugas 12

--- SanberApp/Tugas 12/Component.js
+++ SanberApp/Tugas 12/Component.js
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Platform, TouchableOpacity, FlatList } from 'react-native';
 import Icon from "react-native-vector-icons/MaterialIcons";
+import Videoitem from "./videoitem"
+import data from "./data.json"
 
 export default class App extends Component {
   render() {
@@ -10,10 +12,48 @@ export default class App extends Component {
         <View style={styles.navBar}>
           <Image source={require("./Images/logo.png")}style={{width: 98, height: 22}} /> 
           <View style={styles.rightNavbar}>
-            <Icon name= "search" size= {25}/>
+            <TouchableOpacity>
+            <Icon style= {styles.navItem} name= "search" size= {25}/>
+            </TouchableOpacity>
+            <TouchableOpacity>
+            <Icon style= {styles.navItem} name= "account-circle" size= {25}/>
+            </TouchableOpacity>
           </View>
           </View>
+      <View style= {styles.body}>
+      <FlatList data={data.items} renderItem={(video) => <Videoitem video={video.item}/>}
+      keyExtractor={(item) => item.id}
+      ItemSeparatorComponent={()=> <View style={{height: 0.5, backgroundColor: "#E5E5E5"}}/>}
+      />
       </View>
+      <View style= {styles.tabBar}>
+        <TouchableOpacity style= {styles.tabItems}>
+          
+            <Icon name= "home" size= {25}/>
+            <Text style={styles.tabTitle}>Home</Text>
+          
+        </TouchableOpacity>
+        <TouchableOpacity style= {styles.tabItems}>
+          
+            <Icon name= "whatshot" size= {25}/>
+            <Text style={styles.tabTitle}>Trending</Text>
+          
+        </TouchableOpacity>
+        <TouchableOpacity style= {styles.tabItems}>
+          
+            <Icon name= "subscriptions" size= {25}/>
+            <Text style={styles.tabTitle}>Subscriptions</Text>
+          
+        </TouchableOpacity>
+        <TouchableOpacity style= {styles.tabItems}>
+          
+            <Icon name= "folder" size= {25}/>
+            <Text style={styles.tabTitle}>Library</Text>
+          
+        </TouchableOpacity>
+      </View>
+      </View>
+
     )
   }
 }
@@ -30,5 +70,32 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
+  },
+  rightNavbar: {
+    flexDirection: "row",
+
+  },
+  navItem: {
+    margin: 25,
+  },
+  body: {
+    flex: 1,
+  },
+  tabBar: {
+    height: 60,
+    backgroundColor: "White",
+    borderTopWidth: 0.5,
+    borderColor: "#E5E5E5",
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  tabItems: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  tabTitle: {
+    fontSize: 11,
+    color: "#3c3c3c",
+    paddingTop: 3,
   }
 });
